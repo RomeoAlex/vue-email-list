40ddae7dab201bf48f2523eb517ea8c262d314b3
RomeoAlex <romeo.alessandro17@gmail.com>
2021-12-15 16:07:44 +0100

    passo uno ricevere risposta dal server

--- js/script.js
+++ js/script.js
@@ -5,3 +5,7 @@ const app =  new Vue ({
         message: 'Ciao sono un messsaggio'
     },
 });
+// Utilizzo axios per avere una risposta dal server
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(risposta){
+    console.log(risposta);
+});
