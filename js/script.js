a84cd30cd5cc1a28f4c86e6a9bab57692414a59c
RomeoAlex <romeo.alessandro17@gmail.com>
2021-12-17 14:49:20 +0100

    completamento

--- js/script.js
+++ js/script.js
@@ -2,10 +2,31 @@ Vue.config.devtools = true;
 const app =  new Vue ({
     el:'#root',
     data: {
-        message: 'Ciao sono un messsaggio'
+        // dichiaro un array vuoto per riempirlo con i get di axios
+        mails: [],
     },
+    created:function() {
+        for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            // utilizzo arrow function
+            .then((risposta) =>{
+                // creo variabile per allocare il dato ricevuto dal server già ridotto all'informazione desiderata dall'array di oggetti ricevuto inizialmente
+                const newMail = risposta.data.response;
+                // pusho il dato nell'array dichiarato nel data di VUe
+                this.mails.push(newMail);
+
 });
+        }
+    }
+});
+// se la lunghezza di mails è inferiore a 10 faccio una richiesta axios
 // Utilizzo axios per avere una risposta dal server
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(risposta){
-    console.log(risposta);
-});
+// let mails = [];
+// let addMAil = axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(risposta){
+//     // in questo modo ho in console la singola email estrapolata dalla richiesta e ricevuta del server
+// // console.log(risposta.data.response);
+// mails.push(risposta.data.response);
+// });
+
+
+
